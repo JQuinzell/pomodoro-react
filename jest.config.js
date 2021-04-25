17b1cf200d059d48d1eb38a1cd15f36e4a21e6b2
jquinzell <>
2021-04-25 18:05:32 -0500

    test timer

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  testEnvironment: 'enzyme',
   rootDir: './',
   roots: ['<rootDir>/src/', '<rootDir>'],
   modulePaths: ['<rootDir>/src/'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules'
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/__mocks__/styleMock.ts'
   },
   resetMocks: true
 }
---
+++ src/components/Timer.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Timer } from './Timer'
+import { act } from 'react-dom/test-utils'
+
+describe('Timer', () => {
+  const timerLengthMinutes = 10
+  const onFinish = jest.fn()
+  const init = () => {
+    render(
+      <Timer timerLengthMinutes={timerLengthMinutes} onFinish={onFinish} />
+    )
+  }
+
+  beforeEach(() => {
+    jest.useFakeTimers()
+  })
+
+  it('displays elapsed time', async () => {
+    init()
+
+    act(() => {
+      jest.advanceTimersByTime(5.5 * 60 * 1000)
+    })
+
+    const text = await screen.findByText('5:30')
+    expect(text).toBeInTheDocument()
+  })
+
+  it('stops and calls finish when time over', async () => {
+    init()
+
+    act(() => {
+      jest.advanceTimersByTime(11 * 60 * 1000)
+    })
+
+    const text = await screen.findByText('10:00')
+    expect(text).toBeInTheDocument()
+    expect(onFinish).toBeCalledTimes(1)
+  })
+})
